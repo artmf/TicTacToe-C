aa398590969fc0e84ab0445efb32347aa1ab27c4
Artur M. Felix <arturmontesanofelix@gmail.com>
2019-05-07 18:12:17 -0400

    This is the first commit with the files created in VS2019

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+void setEnv() {
+	int value = 1;
+
+	cout << "     |     |     " << endl;
+	cout << "  " << value++ << "  |";
+	cout << "  " << value++ << "  |";
+	cout << "  " << value++ << "  ";
+	cout << endl << "-----|-----|-----" << endl;
+	cout << "  " << value++ << "  |";
+	cout << "  " << value++ << "  |";
+	cout << "  " << value++ << "  ";
+	cout << endl << "-----|-----|-----" << endl;
+	cout << "  " << value++ << "  |";
+	cout << "  " << value++ << "  |";
+	cout << "  " << value++ << "  " << endl;
+	cout << "     |     |     ";
+}
+
+int main() {
+	int choice;
+	bool exit = false;
+
+	setEnv();
+
+	do {
+
+		cout << ""
+
+		switch (choice)
+		{
+		default:
+			break;
+		}
+	} while (exit != true);
+}
